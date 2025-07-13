1b4cc8663d5ee8c33cc982662f7dfeb6ce98a787
nguyenanhcd <anh487303@gmail.com>
2025-07-13 21:38:54 +0700

    responsive: add responsive to sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,6 @@
 'use client'
-
-import { LogOut } from 'lucide-react'
-import { sidebarItems } from '@/constant/sideBarItems'
+import SideBarDesktop from './SideBarDesktop'
+import SideBarMobile from './SideBarMobile'
 
 interface SideBarProps {
   isCollapsed: boolean
@@ -9,42 +8,14 @@ interface SideBarProps {
 
 const SideBar = ({ isCollapsed }: SideBarProps) => {
   return (
-    <aside
-      className={`fixed top-20 h-[calc(100%-4rem)] left-0 z-10 ${
-        isCollapsed ? 'w-16' : 'w-64'
-      } bg-main border-r border-slate-700 flex flex-col justify-between transition-all duration-300`}
-    >
-      <div className='flex-1'>
-        {/* Navigation */}
-        <nav className=' mt-10 p-2 space-y-3'>
-          {sidebarItems.map((item, index) => (
-            <div
-              key={index}
-              className={`flex items-center px-3 py-2 rounded-lg cursor-pointer transition-colors ${
-                item.active
-                  ? 'bg-default text-primary'
-                  : 'text-white-primary hover:bg-slate-700 hover:text-white'
-              }`}
-            >
-              <item.icon className='h-5 w-5' />
-              {!isCollapsed && (
-                <span className='ml-3 text-sm font-medium'>{item.label}</span>
-              )}
-            </div>
-          ))}
-        </nav>
+    <div>
+      <div className='hidden xl:block'>
+        <SideBarDesktop isCollapsed={isCollapsed} />
       </div>
-
-      {/* Logout */}
-      <div className='p-2 transform -translate-y-30'>
-        <div className='flex items-center px-3 py-2 rounded-lg cursor-pointer text-slate-300 hover:bg-slate-700 hover:text-white transition-colors'>
-          <LogOut className='h-5 w-5' />
-          {!isCollapsed && (
-            <span className='ml-3 text-sm font-medium'>Logout</span>
-          )}
-        </div>
+      <div className='xl:hidden'>
+        <SideBarMobile />
       </div>
-    </aside>
+    </div>
   )
 }
 
