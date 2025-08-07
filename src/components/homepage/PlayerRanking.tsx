bc56ffd35174f94f1dba5178e1e12506d8d9ef34
nguyennanhcd <anh487303@gmail.com>
2025-08-07 17:33:21 +0700

    feat: add swiper to player ranking

--- src/components/homepage/PlayerRanking.tsx
+++ src/components/homepage/PlayerRanking.tsx
@@ -1,13 +1,19 @@
 'use client'
+
 import { ChevronLeft, ChevronRight, Swords } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
 import { playersRank } from '@/constant/playersRank'
-import { PlayerCard } from '@/components/PlayerCard'
+import { PlayerCard } from '../PlayerCard'
 
 const PlayerRanking = () => {
   return (
-    <div className=' xl:py-10 bg-main p-6 text-white rounded-sm'>
+    <div className='xl:py-10 bg-main p-6 text-white rounded-sm'>
       <div className='mb-10 flex items-center justify-between'>
         <h2 className='flex items-center gap-2 text-2xl font-bold'>
           <Swords className='h-6 w-6' />
@@ -16,27 +22,49 @@ const PlayerRanking = () => {
         <div className='flex gap-2'>
           <Button
             size='icon'
-            className=' bg-default text-white hover:bg-default-hover'
+            className='bg-default text-white hover:bg-default-hover player-swiper-button-prev'
           >
             <ChevronLeft className='h-5 w-5' />
           </Button>
           <Button
             size='icon'
-            className='bg-default text-white hover:bg-default-hover'
+            className='bg-default text-white hover:bg-default-hover player-swiper-button-next'
           >
             <ChevronRight className='h-5 w-5' />
           </Button>
         </div>
       </div>
 
-      <ScrollArea className='w-full whitespace-nowrap pb-4 mt-5'>
-        <div className='flex space-x-6'>
-          {playersRank.map((player, index) => (
-            <PlayerCard key={index} {...player} />
-          ))}
-        </div>
-        <ScrollBar orientation='horizontal' className='mt-4' />
-      </ScrollArea>
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={16}
+        slidesPerView={1.2}
+        breakpoints={{
+          640: {
+            slidesPerView: 2
+          },
+          768: {
+            slidesPerView: 2.5
+          },
+          1024: {
+            slidesPerView: 3
+          },
+          1280: {
+            slidesPerView: 4
+          }
+        }}
+        className='w-full pb-4 mt-5'
+        navigation={{
+          nextEl: '.player-swiper-button-next',
+          prevEl: '.player-swiper-button-prev'
+        }}
+      >
+        {playersRank.map((player, index) => (
+          <SwiperSlide key={index}>
+            <PlayerCard {...player} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <div className='mt-8 flex justify-center'>
         <Button className='rounded-md bg-default hover:bg-default-hover'>
